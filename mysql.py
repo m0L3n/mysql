571ebb8c0b3936e0e8ad4b2c8db31f0869bd1e19
m0L3n <m.lenz@mailbox.org>
2023-01-05 19:23:31 +0100

    add project mysql

---
+++ mysql.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import MySQLdb
+
+### INSERT Operation ###
+
+# DB connection
+db = MySQLdb.connect("")
+#cursor object
+cursor = db.cursor()
+# query to inserc record to db
+sql = """ INSERT INTO STUDENT(NAME,SURENAME, ROLL_NO)
+            VALUES('Torsten','Mueller',1)"""
+try:
+    #exec query
+    cursor.execute(sql)
+    #commit to db
+    db.commit()
+
+### READ Operation ###
+
+
+
